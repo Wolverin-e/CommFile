6adedf9751405304e2f19ecf7f8ed1566b0f4bb2
Mitul <patel.6@iitj.ac.in>
2021-04-24 13:14:58 +0000

    Updated new mailing functionalities. - Added dynamic email addressing mode. - Added Support for File sending.

--- MailFile/mailfs.py
+++ MailFile/mailfs.py
@@ -1,11 +1,16 @@
 from __future__ import print_function, absolute_import, division
 from collections import defaultdict
 from errno import ENOENT
-from stat import S_IFREG
+import logging
+from stat import S_IFDIR, S_IFREG
 from time import time
-from fuse import FuseOSError, Operations, LoggingMixIn
+from fuse import FuseOSError, Operations, LoggingMixIn, fuse_get_context
+from pathlib import Path
 
 from .mailing import send_msg
+
+
+logger = logging.getLogger(__name__)
 
 
 class MailFS(LoggingMixIn, Operations):
@@ -15,21 +20,43 @@ class MailFS(LoggingMixIn, Operations):
         self.data = defaultdict(bytes)
         self.fd = 0
         now = time()
+        uid, gid, pid = fuse_get_context()
 
         self.files['/'] = dict(
-            st_mode=(S_IFREG | 0o755),
+            st_mode=(S_IFDIR | 0o755),
             st_ctime=now,
             st_mtime=now,
             st_atime=now,
-            st_size=len("%s\n" % 0)
+            st_uid=uid,
+            st_gid=gid,
+            st_pid=pid,
+            st_nlink=2
         )
 
     def getattr(self, path, fh=None):
-        print(path)
+        super().__getattribute__
         if path not in self.files:
             raise FuseOSError(ENOENT)
 
         return self.files[path]
+
+    def mkdir(self, path, mode):
+
+        now = time()
+        uid, gid, pid = fuse_get_context()
+
+        self.files[path] = dict(
+            st_mode=(S_IFDIR | 0o755),
+            st_ctime=now,
+            st_mtime=now,
+            st_atime=now,
+            st_uid=uid,
+            st_gid=gid,
+            st_pid=pid,
+            st_nlink=2
+        )
+
+        logging.debug(self.files)
 
     def read(self, path, size, offset, fh):
         encoded = lambda x: ('%s\n' % x).encode('utf-8')  # noqa: E731
@@ -38,6 +65,32 @@ class MailFS(LoggingMixIn, Operations):
 
         raise RuntimeError('unexpected path: %r' % path)
 
+    def readdir(self, path, fh):
+        files = ['.', '..']
+        for k in self.files:
+            if k != '/':
+                files.append((Path(k).name, self.getattr(k), 0))
+
+        return files
+
+    def create(self, path, mode, fi=None):
+
+        now = time()
+        uid, gid, pid = fuse_get_context()
+
+        self.files[path] = dict(
+            st_mode=(mode or S_IFREG | 0o755),
+            st_ctime=now,
+            st_mtime=now,
+            st_atime=now,
+            st_uid=uid,
+            st_gid=gid,
+            st_pid=pid,
+            st_size=0
+        )
+
+        return 0
+
     def write(self, path, data, offset, fh):
         self.data[path] = self.data[path][:offset] + data
         self.files[path]['st_size'] = len(self.data[path])
@@ -45,20 +98,29 @@ class MailFS(LoggingMixIn, Operations):
 
     def release(self, path, fh):
 
-        msg = self.data[path].decode("utf-8")
+        path = Path(path)
 
-        send_msg(msg)
+        if(path.suffix == ".mail"):
+            msg = self.data[path.__str__()].decode("utf-8")
+            logging.debug("Suffix mail: %s", self.data)
+            send_msg(
+                msg_body=msg,
+                to=path.parent.name,
+                subject=path.stem
+            )
+        else:
+            attachment = self.data[path.__str__()].decode("utf-8")
+            send_msg(
+                to=path.parent.name,
+                subject=path.name,
+                attach=attachment,
+                filename=path.name
+            )
 
         self.data.clear()
 
-        return super().release(path, fh)
-
     # Disable unused operations
-    access = None
-    flush = None
     getxattr = None
     listxattr = None
     open = None
-    opendir = None
-    releasedir = None
     statfs = None
--- MailFile/mailing.py
+++ MailFile/mailing.py
@@ -3,33 +3,40 @@ import ssl
 from email.message import EmailMessage
 import json
 from pathlib import Path
+from logging import getLogger
 
 FILE_DIR = Path(__file__).parent.resolve()
 
+logger = getLogger(__name__)
 
-def send_msg(msg_body):
+
+def send_msg(msg_body='', to=None, subject=None, attach=None, filename=''):
+
+    logger.debug("-> send_msg %s %s %s %s", msg_body, to, subject, attach)
 
     with open(FILE_DIR/"config.json") as conf_file:
         config = json.load(conf_file)
 
-    msg = EmailMessage()
-    msg.set_content(msg_body)
+    mail = EmailMessage()
+    mail.set_content(msg_body)
 
-    msg['Subject'] = 'Sent using MailFS'
-    msg['From'] = config["from"]
-    msg['To'] = config["send_to"]
+    mail['From'] = config["from"]
+    mail['To'] = to or config["send_to"]
+    mail['Subject'] = subject or 'Sent using MailFS'
 
-    context = ssl.create_default_context()
+    if attach:
+        mail.add_attachment(attach, filename=filename)
 
     smtp_config = config["smtp"]
-
     with(
         smtplib.SMTP_SSL(
             host=smtp_config["server"],
             port=int(smtp_config["port"]),
-            context=context
+            context=ssl.create_default_context()
         ) as server
     ):
 
         server.login(smtp_config["user"], smtp_config["pass"])
-        server.send_message(msg)
+        logger.debug(mail)
+        server.send_message(mail)
+        logger.debug("Sent Mail!")
